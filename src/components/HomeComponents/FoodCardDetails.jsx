a87c1cc77ad1c91ebe887407b3ee6f324445e628
Iffarur <iffarurrahat@gmail.com>
2023-11-09 18:00:52 +0600

    added new section on mysite

--- src/components/HomeComponents/FoodCardDetails.jsx
+++ src/components/HomeComponents/FoodCardDetails.jsx
@@ -1,14 +1,33 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const FoodCardDetails = () => {
 
-    const food = useLoaderData();
+    const [food, setFood] = useState([]);
+
+    const { date, location, name, notes, photo, quantity, status } = food || {}
+
+    const foods = useLoaderData();
+    const { id } = useParams();
+
+    useEffect(() => {
+        const findFood = foods.find(data => data._id === id)
+        setFood(findFood)
+    }, [foods, id])
+
     console.log(food);
 
     return (
-        <div>
-            <p>Details Page</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur praesentium commodi corrupti, enim asperiores placeat odit amet vero incidunt odio esse cumque ad ipsum minima culpa ea iusto aut? Maxime?</p>
+        <div className="max-w-screen-xl mx-auto px-2 md:px-4 lg:px-1 my-5 md:my-10 lg:my-20">
+            <div className="">
+                <img src={photo} alt="" className="mb-4" />
+                <div className="bg-slate-100 p-4">
+                    <h4 className="text-2xl font-bold mb-2">Food Name: {name}</h4>
+                    <p className="font-bold py-1">Location: {location}</p>
+                    <p className="font-bold py-1">Quantity: {quantity}</p>
+                    <p className="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">{notes}</p>
+                </div>
+            </div>
         </div>
     );
 };
